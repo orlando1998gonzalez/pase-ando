42fc2376bb931af41daa66d85832943e10dec023
orlando1998gonzalez <54719769+orlando1998gonzalez@users.noreply.github.com>
2019-11-05 22:14:22 -0600

    cambio prb

--- app/src/main/java/com/example/loginapplication/RegistrateActivity.java
+++ app/src/main/java/com/example/loginapplication/RegistrateActivity.java
@@ -68,7 +68,11 @@ public class RegistrateActivity extends AppCompatActivity implements View.OnClic
         //final int edad = Integer.parseInt(edad.getText().toString());
 
 
-        final String aux = nombre+" "+correo+" "+contrasenia+"";
+        final String aux = nombre+" "+correo+" "+contrasenia+""+"hahaha";
+
+
+
+        
         mTextViewBlanco.setText(aux);
 
     }
